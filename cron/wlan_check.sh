2518863bff364881602652a6f0a167aa460f4282
Sungjin Han <meinside@gmail.com>
2013-02-06 10:38:13 +0900

    edited cron/wlan_check.sh

--- cron/wlan_check.sh
+++ cron/wlan_check.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 # cron script for checking wlan connectivity
+# last update: 2013.02.06.
+
 IP_FOR_TEST="8.8.8.8"
 PING_COUNT=5
-
 PING="/bin/ping"
+
 IFUP="/sbin/ifup"
 IFDOWN="/sbin/ifdown"
-
 INTERFACE="wlan0"
+
+FILES_TO_BE_DELETED="/var/run/avahi-daemon/disabled-for-unicast-local"
 
 # ping test
 $PING -c $PING_COUNT $IP_FOR_TEST > /dev/null
@@ -16,6 +19,10 @@ if [ $? -ge 1 ]
 then
 	echo "$INTERFACE seems to be down, trying to bring it up..."
 
+	# delete files
+	sudo rm -f $FILES_TO_BE_DELETED
+
+	# bring down and up the interface
 	$IFDOWN $INTERFACE
 	sleep 10
 	$IFUP $INTERFACE
