7ef7ea7c0ecf572250780f042a7562de381dfd33
Sungjin Han <meinside@gmail.com>
2013-05-21 21:10:31 +0900

    edited cron/wlan_check.sh

--- cron/wlan_check.sh
+++ cron/wlan_check.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # cron script for checking wlan connectivity
-# last update: 2013.02.07.
+# (should be run as root)
+#
+# last update: 2013.05.21.
 
 IP_FOR_TEST="8.8.8.8"
 PING_COUNT=5
 PING="/bin/ping"
 
-IFUP="/sbin/ifup"
 IFDOWN="/sbin/ifdown"
 INTERFACE="wlan0"
+SERVICE="networking"
 
 AVAHI_DISABLED_FILE="/var/run/avahi-daemon/disabled-for-unicast-local"
 
@@ -19,10 +21,13 @@ if [ $? -ge 1 ];
 then
 	echo "$INTERFACE seems to be down, trying to bring it up..."
 
-	# bring down and up the interface
-	$IFDOWN $INTERFACE
+	# bring down the interface
+	sudo $IFDOWN $INTERFACE
+
 	sleep 10
-	$IFUP $INTERFACE
+
+	# restart networking service
+	sudo service $SERVICE restart
 
 	# restart avahi-daemon
 	if [ -f $AVAHI_DISABLED_FILE ];
