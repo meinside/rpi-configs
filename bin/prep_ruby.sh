1455d69d66f4b2ac9eca016774a4ae1d78a4b3a8
Sungjin Han <meinside@gmail.com>
2014-06-26 21:04:38 +0900

    edit bin/prep_ruby.sh

--- bin/prep_ruby.sh
+++ bin/prep_ruby.sh
@@ -5,7 +5,7 @@
 # for setting up environment for ruby on Raspberry Pi
 # (https://raw.github.com/meinside/raspiconfigs/master/bin/prep_ruby.sh)
 # 
-# last update: 2013.12.29.
+# last update: 2014.06.26.
 # 
 # by meinside@gmail.com
 
@@ -14,9 +14,6 @@ echo -e "\033[32mThis script will help setting up rvm on this Raspberry Pi\033[0
 # install RVM for multi-users
 echo -e "\033[33m>>> installing RVM and Ruby for multi-users...\033[0m\n"
 curl -#L https://get.rvm.io | sudo bash -s stable --autolibs=3 --ruby
-
-# install additional packages
-sudo apt-get install -y libxml2-dev libxslt1-dev
 
 # setting up permissions
 sudo /usr/sbin/usermod -a -G rvm $USER
