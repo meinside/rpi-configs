5062d89623373be58cce806539ddf1b8e461022e
Sungjin Han <meinside@gmail.com>
2016-08-16 09:44:46 +0900

    Edit bin/prep_go.sh (go version 1.7)

--- bin/prep_go.sh
+++ bin/prep_go.sh
@@ -8,7 +8,7 @@
 # (or, can get prebuilt packages at: http://dave.cheney.net/unofficial-arm-tarballs)
 # 
 # created on : 2014.07.01.
-# last update: 2016.07.20.
+# last update: 2016.08.16.
 # 
 # by meinside@gmail.com
 
@@ -21,7 +21,7 @@ BOOTSTRAP_BRANCH="release-branch.go1.4"
 
 # XXX - edit for different version of Go (see: https://go.googlesource.com/go/+refs)
 #INSTALL_BRANCH="release-branch.go1.6"	# branch
-INSTALL_BRANCH="go1.6.3"	# tag
+INSTALL_BRANCH="go1.7"	# tag
 
 function prep {
 	# install essential packages
