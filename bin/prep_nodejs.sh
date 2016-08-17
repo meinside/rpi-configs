a8b2059ecfad06b7aa57ab29c17ccb6fb3547fb8
Sungjin Han <meinside@gmail.com>
2016-08-17 09:29:52 +0900

    Edit bin/prep_nodejs.sh (node version 4.5.0)

--- bin/prep_nodejs.sh
+++ bin/prep_nodejs.sh
@@ -6,11 +6,11 @@
 # from: https://nodejs.org/dist
 # 
 # created on : 2013.07.19.
-# last update: 2016.06.29.
+# last update: 2016.08.17.
 # 
 # by meinside@gmail.com
 
-VERSION="4.4.7"	# XXX - edit this for other versions
+VERSION="4.5.0"	# XXX - edit this for other versions
 
 NODEJS_DIST_BASEURL="https://nodejs.org/dist"
 TEMP_DIR="/tmp"
