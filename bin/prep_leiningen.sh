b1eaca0a0ab2a145e744af9e07a505b1fb355d75
Sungjin Han <meinside@gmail.com>
2016-12-28 19:53:59 +0900

    Add bin/prep_leiningen.sh and edit .gitignore

---
+++ bin/prep_leiningen.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# prep_leiningen.sh
+# 
+# for downloading and setting up 'lein'
+# 
+# last update: 2016.12.28.
+# 
+# by meinside@gmail.com
+
+# XXX - for making newly created files/directories less restrictive
+umask 0022
+
+# colors
+RED="\033[0;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+RESET="\033[0m"
+
+LEIN_SRC="https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein"
+LEIN_BIN="/usr/local/bin/lein"
+
+function prep {
+	# install needed packages
+	sudo apt-get install -y oracle-java8-jdk
+}
+
+function install {
+	sudo wget "$LEIN_SRC" -O "$LEIN_BIN"
+	sudo chown $USER.$USER "$LEIN_BIN"
+	sudo chmod uog+x "$LEIN_BIN"
+
+	echo -e "${GREEN}>>> ${LEIN_BIN} was installed.${RESET}"
+}
+
+prep
+
+install
+
