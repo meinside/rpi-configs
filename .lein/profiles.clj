27be057a8fcc16635b9a9146ed2ab92a2e63479b
Sungjin Han <meinside@gmail.com>
2017-01-12 12:17:57 +0900

    Edit .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,8 @@
-;; last update: 2016.12.28.
+;; last update: 2017.01.12.
 
-{:user {:plugins [[lein-exec "0.3.6"]]}}
+{
+  :user {
+    :plugins [[lein-exec "0.3.6"]]
+    :repl-options {:init (set! *print-length* 100)}
+  }
+}
