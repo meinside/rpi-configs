f647dd60d86ae4e4c573ce5f1b6c0d39d182a44a
Sungjin Han <meinside@gmail.com>
2017-04-13 15:48:17 +0900

    Add bin/prep_nvim.sh

---
+++ bin/prep_nvim.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# prep_nvim.sh
+# 
+# for building neovim from source code on Raspberry Pi
+#
+# last update: 2017.04.13.
+# 
+# by meinside@gmail.com
+
+# XXX - for making newly created files/directories less restrictive
+umask 0022
+
+TMP_DIR=/tmp/nvim
+
+NVIM_VERSION="v0.1.7"
+
+function prep {
+	# install needed packages
+	sudo apt-get install -y libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+
+	clean
+}
+
+function clean {
+	rm -rf $TMP_DIR
+}
+
+function install {
+	git clone https://github.com/neovim/neovim.git $TMP_DIR
+
+	cd $TMP_DIR
+	git checkout $NVIM_VERSION
+
+	# configure and build
+	make CMAKE_BUILD_TYPE=Release
+
+	# install
+	sudo make install
+}
+
+prep
+install
+clean
+
