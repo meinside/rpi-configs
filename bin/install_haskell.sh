2846d90866a5452cf955842bdb9fa5098b0916af
Sungjin Han <meinside@gmail.com>
2017-09-07 10:30:52 +0900

    Edit bin/install_haskell.sh

--- bin/install_haskell.sh
+++ bin/install_haskell.sh
@@ -5,7 +5,7 @@
 # for installing haskell stack
 # (referenced: http://allocinit.io/haskell/haskell-on-raspberry-pi-3/)
 # 
-# last update: 2017.08.16.
+# last update: 2017.09.07.
 # 
 # by meinside@gmail.com
 
@@ -19,7 +19,7 @@ YELLOW="\033[0;33m"
 RESET="\033[0m"
 
 # prebuilt stack binary
-STACK_BIN="https://github.com/commercialhaskell/stack/releases/download/v1.5.0/stack-1.5.0-linux-arm.tar.gz"
+STACK_BIN="https://github.com/commercialhaskell/stack/releases/download/v1.5.1/stack-1.5.1-linux-arm.tar.gz"
 
 TMP_DIR="/tmp"
 LOCAL_BIN_DIR="/usr/local/bin"
@@ -27,25 +27,21 @@ LOCAL_BIN_DIR="/usr/local/bin"
 function prep {
 	# install needed packages
 	sudo apt-get install -y g++ gcc libc6-dev libffi-dev libgmp-dev make xz-utils zlib1g-dev git gnupg llvm-3.7
-	sudo ln -sf /usr/bin/opt-3.7 /usr/bin/opt
-	sudo ln -sf /usr/bin/llc-3.7 /usr/bin/llc
 }
 
 # download stack binary
 function download_stack {
-	cd $TMP_DIR
-	wget $STACK_BIN -O "stack.tgz"
-	mkdir -p stack
-	tar -xzvf "stack.tgz" -C stack --strip-components=1
-	sudo cp stack/stack $LOCAL_BIN_DIR
-
+	cd $TMP_DIR && \
+	wget $STACK_BIN -O "stack.tgz" && \
+	mkdir -p stack && \
+	tar -xzvf "stack.tgz" -C stack --strip-components=1 && \
+	sudo cp stack/stack $LOCAL_BIN_DIR && \
 	echo -e "${GREEN}>>> stack was installed in $LOCAL_BIN_DIR.${RESET}"
 }
 
 # install ghc & ghci
 function install {
-	stack update && stack setup
-
+	stack update && stack setup && \
 	echo -e "${GREEN}>>> ghc and ghci were installed.${RESET}"
 }
 
