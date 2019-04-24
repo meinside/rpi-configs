5c4d0b0ea7e9dcf3dbe8a2c41ec157ac5b46e5a6
Sungjin Han <meinside@gmail.com>
2019-04-24 21:25:37 +0900

    Restore .lein/profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,11 @@
+;; My .lein/profiles.clj
+;;
+;; last update: 2019.04.24.
+
+;; Plugins and dependencies
+;;
+;; https://github.com/technomancy/leiningen/tree/master/lein-pprint
+;; https://github.com/technomancy/slamhound
+
+{:user {:plugins [[lein-pprint "1.2.0"]]
+        :dependencies [[slamhound "1.5.5"]]}}
