ba7617e99e3882d003893b7761d9695b8ebb0f7d
Sungjin Han <meinside@gmail.com>
2019-04-25 15:31:33 +0900

    Edit .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,12 @@
 ;; My .lein/profiles.clj
 ;;
-;; last update: 2019.04.24.
+;; last update: 2019.04.25.
 
 ;; Plugins and dependencies
 ;;
+;; https://github.com/kumarshantanu/lein-exec
 ;; https://github.com/technomancy/leiningen/tree/master/lein-pprint
 ;; https://github.com/technomancy/slamhound
 
-{:user {:plugins [[lein-pprint "1.2.0"]]
+{:user {:plugins [[lein-exec "0.3.7"], [lein-pprint "1.2.0"]]
         :dependencies [[slamhound "1.5.5"]]}}
