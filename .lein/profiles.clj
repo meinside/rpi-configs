424b28d59004a290e630558baa3f385180451258
Sungjin Han <meinside@gmail.com>
2019-07-29 20:18:07 +0900

    Edit .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,13 +1,29 @@
-;; My .lein/profiles.clj
+;; .lein/profiles.clj
 ;;
-;; last update: 2019.07.28.
+;; last update: 2019.07.29.
 
-;; Plugins and dependencies
-;;
-;; https://github.com/kumarshantanu/lein-exec
-;; https://github.com/technomancy/leiningen/tree/master/lein-pprint
-;; https://github.com/technomancy/slamhound
-;; https://github.com/clojure-emacs/cider-nrepl
+{:user {
+        ;; plugins
+        :plugins [
+                  ;; https://github.com/kumarshantanu/lein-exec
+                  [lein-exec "0.3.7"]
 
-{:user {:plugins [[lein-exec "0.3.7"], [lein-pprint "1.2.0"], [cider/cider-nrepl "0.21.1"]]
-        :dependencies [[slamhound "1.5.5"]]}}
+                  ;; https://github.com/technomancy/leiningen/tree/master/lein-pprint
+                  [lein-pprint "1.2.0"]
+
+                  ;; https://github.com/clojure-emacs/cider-nrepl
+                  [cider/cider-nrepl "0.21.1"]
+
+                  ;; https://github.com/venantius/ultra
+                  ;;[venantius/ultra "0.6.0"]
+
+                  ;; https://github.com/xsc/lein-ancient
+                  [lein-ancient "0.6.15"]]
+
+        ;; dependencies
+        :dependencies [
+                       ;; https://github.com/technomancy/slamhound
+                       [slamhound "1.5.5"]]
+
+        ;; global variables
+        :global-vars {*print-length* 20}}}
